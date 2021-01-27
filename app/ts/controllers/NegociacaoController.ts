2682cc0fc9fba02c27ab6bb43d2bdafdf49c5560
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2021-01-27 10:31:36 -0300

    Trabalhando com Enuns

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -24,6 +24,13 @@ export class NegociacaoController {
 
         event.preventDefault();
 
+        let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+        if (!this._ehDiaUtil(data)) {
+            this._mensagemView.update('Somente negociações em dias úteis, por favor.');
+            return
+        }
+
         const negociacao = new Negociacao(
             new Date(this._inputData.val().replace(/-/g, ',')), 
             parseInt(this._inputQuantidade.val()),
@@ -34,4 +41,18 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação realizada com sucesso!');
     }
+
+    private _ehDiaUtil(data: Date) {
+        return data.getDay() != DiaDaSemana.Sabado && data.getDay() != DiaDaSemana.Domingo;
+    }
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Terca,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
 }
