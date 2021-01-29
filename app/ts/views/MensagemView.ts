a525ff7eba6155d4bc64a0cafe85c63509b3685b
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2021-01-29 11:52:38 -0300

    Consumindo API externa utilizando fetch para importar os dados dessa API

--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -4,6 +4,6 @@ export class MensagemView extends View<string> {
 
     template(model: string): string{
 
-        return `<p class="alert alert-info">${model}</p>`;
+        return `<p class="alert alert-success">${model}</p>`;
     }
 }
